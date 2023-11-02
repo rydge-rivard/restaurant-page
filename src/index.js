55132b54a30b7535fa8489d1eb5f7ce0146e5226
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-02 08:11:47 +0000

    Create page load  DOM functions and pass to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import {createH1} from './page-load.js';
+import {createImg, createTextElement} from './page-load.js';
 
-createH1();
+const pageLoadElements = (function () {
+    const divContent = document.querySelector('#content')
+    const p = document.createElement('p');
+    const pText = `Milliways, better known as the Restaurant at the End of the Universe, 
+    is a five star restaurant situated at the end of time and matter. 
+    Its main attraction is allowing diners to view a Gnab Gib, 
+    before desserts are served.`;
+    
+    const h1 = document.createElement('h1');
+    const h1Text = 'Milliways';
+
+    const img = document.createElement('img');
+
+    createTextElement(h1, h1Text, divContent);
+    createTextElement(p, pText, divContent);
+
+    createImg(img, './bar.jpg', '800', 'Bar with wooden walls.', divContent);
+})();
--- src/page-load.js
+++ src/page-load.js
@@ -1,33 +1,13 @@
-export {createH1};
+export {createTextElement, createImg};
 
-// const pageLoadElements = (function () {
-//     const img = document.createElement('img');
-//     const p = document.createElement('p');
-//     const divContent = document.querySelector('#content')
-//     console.log('init working')
+const createTextElement = (element, text, parent) => {
+    element.textContent = text;
+    parent.appendChild(element);
+}
 
-//     function createImg () {
-//         divContent.appendChild(img);
-//     }
-
-//     function createP () {
-//         p.textContent = `Milliways, better known as the Restaurant at the End of the Universe, 
-//         is a five star restaurant situated at the end of time and matter. 
-//         Its main attraction is allowing diners to view a Gnab Gib, 
-//         before desserts are served.`;
-//         divContent.appendChild(p);
-//     }
-
-//     return {
-//         createP: createP,
-//         createImg: createImg,
-//     }
-// })();
-
-const createH1 = () => {
-    const divContent = document.querySelector('#content')
-    const h1 = document.createElement('h1');
-    console.log('enter function')
-    h1.textContent = 'Milliways';
-    divContent.appendChild(h1);
+const createImg = (element, src, width, alt, parent) => {
+    element.setAttribute("src", src);
+    element.setAttribute("width", width);
+    element.setAttribute("alt", alt);
+    parent.appendChild(element);
 }
