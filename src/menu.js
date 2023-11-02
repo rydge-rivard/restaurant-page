264f3cd825a41662434aa3d91115ec630b8f5560
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-11-02 11:43:24 +0000

    Populate remaining menus items.

--- src/menu.js
+++ src/menu.js
@@ -20,3 +20,27 @@ function newMenuItem (cardNumber, item, src, alt, ingredients) {
 newMenuItem(card1, 'Space Burger', './img/hamburger.svg', 'Pasta graphic', 
 'juicy spacecow, milky way mayo, asteroid lettuce');
 
+const card2 = createContainer(menuContainer, 'card');
+
+newMenuItem(card2, 'Space Croissant', './img/food-croissant.svg', 'Croissant graphic', 
+'flaky, buttery, delicious');
+
+const card3 = createContainer(menuContainer, 'card');
+
+newMenuItem(card3, 'Mystery Drumstick', './img/food-drumstick.svg', 'Drumstick graphic', 
+'pan fried, sweet meat, origin unknown');
+
+const card4 = createContainer(menuContainer, 'card');
+
+newMenuItem(card4, 'Galaxy Noodles', './img/noodles.svg', 'Bowl of noodles graphic', 
+'famous galactic five spice, beef broth, hot chilis');
+
+const card5 = createContainer(menuContainer, 'card');
+
+newMenuItem(card5, 'Moon Dog', './img/food-hot-dog.svg', 'Hot dog graphic', 
+'charcoal grilled, toasted bun, fried onions');
+
+const card6 = createContainer(menuContainer, 'card');
+
+newMenuItem(card6, 'Earth Turkey', './img/food-turkey.svg', 'Cooked turkey graphic', 
+'slow roasted, crispy, succulent');
